e239e9ba7bf8f0bdbbed5ea3e67ad2d8a087784e
AnshMNSoni <anshsoni702@gmail.com>
2024-10-24 00:28:05 +0530

    Code for undirectionl and directional Graph + Matrix

--- graph.py
+++ graph.py
@@ -1,90 +1,131 @@
 import networkx as nx
-from tkinter import simpledialog
+from tkinter import simpledialog, messagebox
+from tkinter import *
 import matplotlib.pyplot as plt
 
 
-def adjacency_matrix(G):
-    length = len(G.vertices)
+class Graph:
+    def __init__(self):
+        pass
     
-    adj = {vertex: [] for vertex in G.vertices}
-    for edge in G.edges:
-        vertex1, vertex2 = edge[0], edge[1]
-        adj[vertex1].append(vertex2)
-        adj[vertex2].append(vertex1)  
+    def adjacency_matrix(self,G):
+        length = len(G.vertices)
         
-    adj_matrix = [[0 for _ in range(length)] for _ in range(length)]
-    
-    for i in range(length):
-        for j in range(length):
-            cnt = adj[G.vertices[i]].count(G.vertices[j])
-            adj_matrix[i][j] = cnt
-     
-    return adj_matrix
-
-
-def undi_edges(vertices):
-    cnt = 0
-    edges = []
-    
-    for i in vertices:
-        for j in range(cnt, len(vertices)):
-            num = simpledialog.askinteger("Input", "Number of edges between " + i + "-" + vertices[j] + ": ")
+        adj = {vertex: [] for vertex in G.vertices}
+        
+        try:
+            for edge in G.edges:
+                vertex1, vertex2 = edge[0], edge[1]
+                adj[vertex1].append(vertex2)
+                adj[vertex2].append(vertex1)  
+                
+            adj_matrix = [[0 for _ in range(length)] for _ in range(length)]
             
-            for _ in range(num):
-                edges.append((i, vertices[j]))
-        cnt += 1
-    return edges
+            for i in range(length):
+                for j in range(length):
+                    cnt = adj[G.vertices[i]].count(G.vertices[j])
+                    adj_matrix[i][j] = cnt
+             
+            return adj_matrix
+        except TypeError:
+            return
 
-
-def undiMatrix(Graph, vertices):
-    edges = undi_edges(vertices)
-    
-    G = Graph(vertices=vertices, edges=edges)
-    
-    matrix = adjacency_matrix(G)
-    return matrix
-
+    def undi_edges(self, vertices):
+        cnt = 0
+        edges = []
         
-def undiGraph(vertices):
-    edges = undi_edges(vertices)
+        for i in vertices:
+            for j in range(cnt, len(vertices)):
+                self.window = Tk()    
+                self.img = PhotoImage(file='./main_bg.png')
+                self.window.label = Label(image=self.img)
+                self.window.label.grid(row=0, column=0)
+                
+                num = simpledialog.askinteger("Input", "Enter the number of edges between " + i + "-" + vertices[j] + ": ")
+                self.window.label = Label(text=num)
+                self.window.label.grid(row=0, column=1)
+                
+                try:
+                    for _ in range(num):
+                        edges.append((i, vertices[j]))
+                except TypeError:
+                    edges = None
+                    messagebox.showerror(title="Try Again!", message="Edges cannot be empty.")
+                finally:
+                    self.window.destroy()
+                    
+            cnt += 1
+        return edges
     
-    plotGraph = nx.Graph()
-    plotGraph.add_nodes_from(vertices)
-    plotGraph.add_edges_from(edges)
-
-    nx.draw(plotGraph, with_labels=True, node_color='skyblue', node_size=700, edge_color='gray', font_size=15, font_weight='bold')
-
-    plt.show()
-
-
-def di_edges(vertices):
-    edges = []
+    
+    def undiMatrix(self, Graph, vertices):
+        edges = self.undi_edges(vertices)
         
-    for i in vertices:
-        for j in range(len(vertices)):
-            num = simpledialog.askinteger("Input", "Number of edges between " + i + "-" + vertices[j] + ": ")
+        G = Graph(vertices=vertices, edges=edges)
+        
+        matrix = self.adjacency_matrix(G)
+        return matrix
+    
             
-            for _ in range(num):
-                edges.append((i, vertices[j]))
-    return edges
-
-
-def diMatrix(Graph, vertices):
-    edges = di_edges(vertices)
-    
-    G = Graph(vertices=vertices, edges=edges)
-    
-    matrix = adjacency_matrix(G)
-    return matrix
+    def undiGraph(self, vertices):
+        edges = self.undi_edges(vertices)
         
-
-def diGraph(vertices):
-    edges = di_edges(vertices)
+        try:
+            plotGraph = nx.Graph()
+            plotGraph.add_nodes_from(vertices)
+            plotGraph.add_edges_from(edges)
+        
+            nx.draw(plotGraph, with_labels=True, node_color='skyblue', node_size=700, edge_color='gray', font_size=15, font_weight='bold')
+        
+            plt.show()
+        except TypeError:
+            return
     
-    plotGraph = nx.DiGraph()
-    plotGraph.add_nodes_from(vertices)
-    plotGraph.add_edges_from(edges)
-
-    nx.draw(plotGraph, with_labels=True, node_color='skyblue', node_size=700, edge_color='gray', font_size=15, font_weight='bold')
-
-    plt.show()
+    
+    def di_edges(self, vertices):
+        edges = []
+            
+        for i in vertices:
+            for j in range(len(vertices)):
+                self.window = Tk()    
+                self.img = PhotoImage(file='./main_bg.png')
+                self.window.label = Label(image=self.img)
+                self.window.label.grid(row=0, column=0)
+                
+                num = simpledialog.askinteger("Input", "Enter the number of edges between " + i + "-" + vertices[j] + ": ")
+                self.window.label = Label(text=num)
+                self.window.label.grid(row=0, column=1)
+                
+                try:
+                    for _ in range(num):
+                        edges.append((i, vertices[j]))
+                except TypeError:
+                    edges = None
+                    messagebox.showerror(title="Try Again!", message="Edges cannot be empty.")
+                finally:
+                    self.window.destroy()
+        return edges
+    
+    
+    def diMatrix(self, Graph, vertices):
+        edges = self.di_edges(vertices)
+        
+        G = Graph(vertices=vertices, edges=edges)
+        
+        matrix = self.adjacency_matrix(G)
+        return matrix
+            
+    
+    def diGraph(self, vertices):
+        edges = self.di_edges(vertices)
+        
+        try:
+            plotGraph = nx.DiGraph()
+            plotGraph.add_nodes_from(vertices)
+            plotGraph.add_edges_from(edges)
+        
+            nx.draw(plotGraph, with_labels=True, node_color='skyblue', node_size=700, edge_color='gray', font_size=15, font_weight='bold')
+        
+            plt.show()
+        except TypeError:
+            return
