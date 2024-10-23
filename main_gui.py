c006eea7b90659362b9561af99370237d63ffae8
AnshMNSoni <anshsoni702@gmail.com>
2024-10-24 00:28:35 +0530

    Code for Main GUI (main file)

--- main_gui.py
+++ main_gui.py
@@ -1,62 +1,127 @@
 from collections import namedtuple
 from tkinter import *
-from tkinter import simpledialog
+from tkinter import simpledialog, messagebox
 from graph import *
 from matrix_gui import *
+
+FONT = ('Times New Roman', 16, 'bold')
+FG = '#161D6F'
+BG = '#F5F4B3'
 
 class GUI:
     def __init__(self):
         self.window = Tk()
         self.window.title("Graph Theory")
-        self.window.config(width=500, height=300)
-        
         self.Graph = namedtuple('Graph', ['vertices', 'edges'])
         
-        undigraph = Button(text="Undirected Graph", command=self.to_undiGraph)
-        undigraph.grid(row=0, column=0)
+        self.img = PhotoImage(file='./main_bg.png')
+        self.label = Label(image=self.img)
+        self.label.config(width=1000, height=600)
+        self.label.grid(row=0, column=0, rowspan=4)
         
-        undimatrix = Button(text="Undirected Matrix", command=self.to_undiMatrix)
-        undimatrix.grid(row=0, column=1)
+        self.undigraph = Button(text="Undirected Graph", command=self.to_undiGraph, font=FONT, bg=BG, fg=FG, bd=3, highlightthickness=0)
+        self.undigraph.grid(row=0, column=0)
         
-        digraph = Button(text="Directed Graph", command=self.to_diGraph)
-        digraph.grid(row=1, column=0)
+        self.undimatrix = Button(text="Undirected Matrix", command=self.to_undiMatrix, font=FONT, bg=BG, fg=FG, bd=3, highlightthickness=0)
+        self.undimatrix.grid(row=1, column=0)
         
-        dimatrix = Button(text="Directed Matrix", command=self.to_diMatrix)
-        dimatrix.grid(row=1, column=1)
+        self.digraph = Button(text="Directed Graph", command=self.to_diGraph, font=FONT, bg=BG, fg=FG, bd=3, highlightthickness=0)
+        self.digraph.grid(row=2, column=0)
+        
+        self.dimatrix = Button(text="Directed Matrix", command=self.to_diMatrix, font=FONT, bg=BG, fg=FG, bd=3, highlightthickness=0)
+        self.dimatrix.grid(row=3, column=0)
         
         self.window.mainloop()
         
         
     def to_undiGraph(self):
-        self.window.destroy()
-        user = simpledialog.askstring("Input", "Enter the vertices of the Graph: ").upper().split()
-        vertices = list(user)
-        
-        undiGraph(vertices)
+        try:
+            self.destroyButtons()
+            user = simpledialog.askstring("Input", "Enter the vertices of the Graph:\n\nNote: Leave empty space while entering vertices\n\nExample: A B C").upper().split()
+            
+            if(user != []):
+                vertices = list(user)
+                self.window.destroy()
+                Graph().undiGraph(vertices)
+            else:
+                messagebox.showwarning(title="Empty Space Found", message="Please, enter atleast one vertex!")
+                self.window.destroy()
+                GUI()
+            
+        except AttributeError:
+            messagebox.showerror(title="Try again", message="Something went wrong.")
+            self.window.destroy()
+            GUI()
         
     
     def to_diGraph(self):
-        self.window.destroy()
-        user = simpledialog.askstring("Input", "Enter the vertices of the Graph: ").upper().split()
-        vertices = list(user)
-        
-        diGraph(vertices)
+        try:
+            self.destroyButtons()
+            user = simpledialog.askstring("Input", "Enter the vertices of the Graph:\n\nNote: Leave empty space while entering vertices\n\nExample: A B C").upper().split()
+            
+            if(user != []):
+                vertices = list(user)
+                self.window.destroy()
+                Graph().diGraph(vertices)
+            else:
+                messagebox.showwarning(title="Empty Space Found", message="Please, enter atleast one vertex!")
+                self.window.destroy()
+                GUI()
+            
+        except AttributeError:
+            messagebox.showerror(title="Try again", message="Something went wrong.")
+            self.window.destroy()
+            GUI()
     
     
     def to_undiMatrix(self):
-        self.window.destroy()
-        user = simpledialog.askstring("Input", "Enter the vertices of the Graph: ").upper().split()
-        vertices = list(user)
-        
-        matrix = undiMatrix(self.Graph, vertices)
-        display_matrix(matrix)
+        try:
+            self.destroyButtons()
+            user = simpledialog.askstring("Input", "Enter the vertices of the Graph:\n\nNote: Leave empty space while entering vertices\n\nExample: A B C").upper().split()
+            
+            if(user != []):
+                vertices = list(user)
+                self.window.destroy()
+                matrix = Graph().undiMatrix(self.Graph, vertices)
+                display_matrix(matrix)
+            else:
+                messagebox.showwarning(title="Empty Space Found", message="Please, enter atleast one vertex!")
+                self.window.destroy()
+                GUI()
+                
+                
+        except AttributeError:
+            messagebox.showerror(title="Try again", message="Something went wrong.")
+            self.window.destroy()
+            GUI()
+    
     
     def to_diMatrix(self):
-        self.window.destroy()
-        user = simpledialog.askstring("Input", "Enter the vertices of the Graph: ").upper().split()
-        vertices = list(user)
+        try:
+            self.destroyButtons()
+            user = simpledialog.askstring("Input", "Enter the vertices of the Graph:\n\nNote: Leave empty space while entering vertices\n\nExample: A B C").upper().split()
+            
+            if(user != []):
+                vertices = list(user)
+                self.window.destroy()
+                matrix = Graph().diMatrix(self.Graph, vertices)
+                display_matrix(matrix)
+            else:
+                messagebox.showwarning(title="Empty Space Found", message="Please, enter atleast one vertex!")
+                self.window.destroy()
+                GUI()
+            
+        except AttributeError:
+            messagebox.showerror(title="Try again", message="Something went wrong.")
+            self.window.destroy()
+            GUI()
         
-        matrix = diMatrix(self.Graph, vertices)
-        display_matrix(matrix)
+        
+    def destroyButtons(self):
+        self.undigraph.destroy()
+        self.digraph.destroy()
+        self.undimatrix.destroy()
+        self.dimatrix.destroy()
+    
     
 GUI()
