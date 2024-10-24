fb6ebb005b66a55691479a74f0b8b7df3f337e43
AnshMNSoni <anshsoni702@gmail.com>
2024-10-24 22:55:20 +0530

    added ploting of B-matrix and Path matrix

--- matrix_gui.py
+++ matrix_gui.py
@@ -5,10 +5,51 @@ FG = '#161D6F'
 BG1 = '#F5F4B3'
 BG2 = '#B9E5E8'
 
+# Displaying Matrix 
 def display_matrix(matrix):
     try:
         window = Tk()
         window.title("Adjacency Matrix")
+        window.config(padx=50, pady=50, bg=BG1)
+    
+        for i in range(len(matrix)):
+            for j in range(len(matrix)):
+                label = Label(text=matrix[i][j], fg=FG, bg=BG2, font=FONT, borderwidth=5, relief='ridge', width=10, height=2)
+                label.config(padx=20, pady=20)
+                label.grid(row=i, column=j)
+                
+        window.mainloop()
+
+    except TypeError:
+        window.destroy()
+        return
+
+
+# Displaying B-Matrix 
+def display_Bmatrix(matrix):
+    try:
+        window = Tk()
+        window.title("B Matrix")
+        window.config(padx=50, pady=50, bg=BG1)
+    
+        for i in range(len(matrix)):
+            for j in range(len(matrix)):
+                label = Label(text=matrix[i][j], fg=FG, bg=BG2, font=FONT, borderwidth=5, relief='ridge', width=10, height=2)
+                label.config(padx=20, pady=20)
+                label.grid(row=i, column=j)
+                
+        window.mainloop()
+
+    except TypeError:
+        window.destroy()
+        return
+    
+
+# Displaying B-Matrix 
+def display_pathMatrix(matrix):
+    try:
+        window = Tk()
+        window.title("B Matrix")
         window.config(padx=50, pady=50, bg=BG1)
     
         for i in range(len(matrix)):
